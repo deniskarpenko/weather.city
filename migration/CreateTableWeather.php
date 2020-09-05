355e2b1d6d7962d115ec5e31f4844220b6d9e81d
Denis Karpenko <new.den84@gmail.com>
2020-09-05 20:36:36 +0300

    add gitignore

---
+++ migration/CreateTableWeather.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\migration;
+
+
+class CreateTableWeather implements Migration
+{
+
+    public static function run(): string
+    {
+        return "CREATE TABLE IF NOT EXISTS `weather` ( city_id varchar(300))";
+    }
+}
