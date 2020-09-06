423a3393c106a20105add8868a1fb50dfc3ab70c
Denis Karpenko <new.den84@gmail.com>
2020-09-06 06:25:09 +0300

    fill Ukraine cities in DB

--- model/db/City.php
+++ model/db/City.php
@@ -2,11 +2,25 @@
 
 namespace app\model\db;
 
+use Exception;
+
 
 class City extends DbModel
 {
     public function fillCitiesInDB($cities)
     {
-
+        if($this->checkPDO()){
+            $stmt = $this->pdo->prepare("INSERT INTO city(id, city) VALUES(?,?)");
+            try {
+                $this->pdo->beginTransaction();
+                    foreach ($cities as $city){
+                        $stmt->execute([$city->id, $city->name]);
+                    }
+                $this->pdo->commit();
+            } catch (Exception $e){
+                $this->pdo->rollback();
+                throw $e;
+            }
+        }
     }
 }
